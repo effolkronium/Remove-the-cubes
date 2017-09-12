96f0c99c11faf2ab6fe92c5d66d57c2a8f1a6bd3
effolkronium <effolkronium@gmail.com>
2017-09-12 17:46:45 +0300

    Add favicon support

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
+    console.log(`LOG: request url === ${req.url}`);
+
+    const {url} = req;
+
+    // app icon
+    if(/favicon/i.test(url)) sendFile('./favicon.png', 'image/png', res);
+
     
+    console.log(`LOG: request is ended`);
 });
 
 server.listen(8080);
+
+// Send file to a client and auto-end responce
+function sendFile(path, contentType, responce) {
+    responce.setHeader('Content-Type', contentType);
+    fs.createReadStream(path).pipe(responce);
+}
